789ed8a7e74f366290a0d7c0481e9d894af2de1e
Foxylab <rushan.shakirzianov@gmail.com>
2022-03-04 18:44:33 +0300

    add popup with card adding, make listeners for open and close popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,4 @@
 let elements = document.querySelector(".elements"); // находим секцию с карточками
-
-let popupProfile = document.querySelector("#popup-profile"); //находим попап
-let formElementProfile = popupProfile.querySelector("#form-profile"); // находим всю форму профайла, пригодится для кнопки Submit
-let openPopupProfile = document.querySelector("#profile-edit-button"); //находим кнопку открытия редактирования профиля
-let closePopupProfile = popupProfile.querySelector("#profile-closer"); //находим кнопку закрытия редактирования профиля
-//let submitButton = popup.querySelector(".popup__button"); //находим кнопку СОХРАНИТЬ
-let authorName = document.querySelector(".profile__name"); //находим элемент имени пользователя
-let authorDescription = document.querySelector(".profile__description"); //находим элемент описания пользователя пользователя
-//let personal = popup.querySelectorAll(".popup__input"); //находим элемент формы описывающий пользователя - бесполезная переменная
-let popupAuthorName = popupProfile.querySelector("#name-profile"); //находим input c именем пользователя
-let popupAuthorDescription = popupProfile.querySelector("#description-profile"); //находим input c описанием пользователя
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -38,27 +26,6 @@ const initialCards = [
   }
 ];
 
-
-//функции
-
-//добавляет класс .popup_opened
-function openedPopupProfile() {
-  popupProfile.classList.add("popup_opened");
-}
-
-//убирает класс .popup_opened
-function closedPopupProfile() {
-  popupProfile.classList.remove("popup_opened");
-}
-
-//решение предложенное платформой
-function formSubmitHandlerProfile(evt) {
-  evt.preventDefault(); //сборс стандартной отправки
-  authorName.textContent = popupAuthorName.value; //обратное действие, в текстовое значение объекта записываем тестовое содержимое песевдомассива input
-  authorDescription.textContent = popupAuthorDescription.value;
-  closedPopupProfile(); //удаляет класс .popup_opened
-}
-
 //add cards function
 function addCards(input) {//описываем функцию добавления карточек
   for (let i = 0; i < input.length; i++) { //перебор массива объектов, затем //мягко, не затрагивая DOM, вставляем новые блоки в секцию elements, вставка происходит всегда после начала
@@ -73,8 +40,45 @@ function addCards(input) {//описываем функцию добавлени
   };
 };
 
-//слушатели
+// заполняем секцию elements
+addCards(initialCards);//вызываем функцию
 
+
+//далее все что касается попапа с профилем пользователя
+
+let popupProfile = document.querySelector("#popup-profile"); //находим попап порфиля
+let formElementProfile = popupProfile.querySelector("#form-profile"); // находим всю форму профайла, пригодится для кнопки Submit
+let openPopupProfile = document.querySelector("#profile-edit-button"); //находим кнопку открытия редактирования профиля
+let closePopupProfile = popupProfile.querySelector("#profile-closer"); //находим кнопку закрытия редактирования профиля
+//let submitButton = popup.querySelector(".popup__button"); //находим кнопку СОХРАНИТЬ
+let authorName = document.querySelector(".profile__name"); //находим элемент имени пользователя
+let authorDescription = document.querySelector(".profile__description"); //находим элемент описания пользователя пользователя
+//let personal = popup.querySelectorAll(".popup__input"); //находим элемент формы описывающий пользователя - бесполезная переменная
+let popupAuthorName = popupProfile.querySelector("#name-profile"); //находим input c именем пользователя
+let popupAuthorDescription = popupProfile.querySelector("#description-profile"); //находим input c описанием пользователя
+
+
+//функции
+
+//добавляет класс .popup_opened
+function openedPopupProfile() {
+  popupProfile.classList.add("popup_opened");
+};
+
+//убирает класс .popup_opened
+function closedPopupProfile() {
+  popupProfile.classList.remove("popup_opened");
+};
+
+//решение предложенное платформой
+function formSubmitHandlerProfile(evt) {
+  evt.preventDefault(); //сборс стандартной отправки
+  authorName.textContent = popupAuthorName.value; //обратное действие, в текстовое значение объекта записываем тестовое содержимое песевдомассива input
+  authorDescription.textContent = popupAuthorDescription.value;
+  closedPopupProfile(); //удаляет класс .popup_opened
+}
+
+//слушатели
 // слушатель по кнопке редактировать профиль, для того что бы открывать попап
 openPopupProfile.addEventListener("click", function () {
   popupAuthorName.value = authorName.textContent;
@@ -100,5 +104,38 @@ popupProfile.addEventListener("click",
 //должно сохранять имя пользователя и описание пользователя в разделе профиль
 formElementProfile.addEventListener("submit", formSubmitHandlerProfile);
 
-// заполняем секцию elements
-addCards(initialCards);
+
+//далее попап с добавлением карточки
+
+let openPopupCard = document.querySelector("#profile-add-button"); //находим кнопку открытия попапа добавления карточки
+let popupCard = document.querySelector("#popup-card"); //находим попап порфиля
+let closePopupCard = popupCard.querySelector("#card-closer"); //находим кнопку закрытия попапа добавления карточки
+
+//функция добавления класса попапу добавления карточки
+function openedPopupCard() {
+  popupCard.classList.add("popup_opened");
+};
+
+//функция изъятия класса у попапа добавления карточки
+function closedPopupCard() {
+  popupCard.classList.remove("popup_opened");
+};
+
+//слушатель кнопки добавления карточки
+openPopupCard.addEventListener("click", openedPopupCard);
+
+//слушатель по кнопке закрыть попап, что бы собственно попап закрывать
+closePopupCard.addEventListener("click", closedPopupCard);
+
+//слушатель по всему попапу, что бы закрывать попап при клике в любом месте, кроме контейнера
+popupCard.addEventListener("click",
+  function (event) {
+    if (event.target === event.currentTarget) {
+      //сравнивает места нажатия кликов, если совпадает целевой клик с расположением обработчика, попап закрывается
+      closedPopupCard();
+    } /*else {
+      openedPopup();
+    }*/
+  },
+  true
+);
