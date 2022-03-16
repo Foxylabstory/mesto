a5d6d2846db4a794ec73e10b99d3ff0aa5a50fa3
Foxylab <rushan.shakirzianov@gmail.com>
2022-03-16 15:41:14 +0300

    refactor under review comments

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,24 @@ const elements = document.querySelector(".elements"); // находим секц
 
 const popupProfile = document.querySelector("#popup-profile"); // находим попап профайла
 const formProfile = popupProfile.querySelector("#form-profile"); // находим форму в попапе профайла
-//const closeProfile = popupProfile.querySelector('#profile-closer');// находим кнопку закрытия попапа профайла
+const closeProfile = popupProfile.querySelector("#profile-closer"); // находим кнопку закрытия попапа профайла
 const authorName = document.querySelector(".profile__name"); // находим имя профиля на странице
 const authorDescription = document.querySelector(".profile__description"); // находим описание профиля на странице
-//const profileEditButton = document.querySelector(".profile__edit-button");// находим кнопку открытия редактирования профиля
+const profileEditButton = document.querySelector(".profile__edit-button"); // находим кнопку открытия редактирования профиля
 const popupProfileAuthorName = popupProfile.querySelector("#name-profile"); // находим инпут для ввода имени профиля
-const popupProfileAuthorDescription = popupProfile.querySelector(
-  "#description-profile"
-); // находим инпут для ввода описания профиля
+const popupProfileAuthorDescription = popupProfile.querySelector("#description-profile"); // находим инпут для ввода описания профиля
 
 const popupCard = document.querySelector("#popup-card"); // находим попап добавления новой карточки
 const formCard = popupCard.querySelector("#form-card"); // находим форму попапа добавления новой карточки
-//const cardAddButton = document.querySelector('#profile-add-button');// находим на странице кнопку добавления новой карточки
-//const closeCard = popupCard.querySelector('#card-closer');// находим кнопку закрытия попапа добавления новой карточки
+const cardAddButton = document.querySelector("#profile-add-button"); // находим на странице кнопку добавления новой карточки
+const closeCard = popupCard.querySelector("#card-closer"); // находим кнопку закрытия попапа добавления новой карточки
 const popupCardHeader = popupCard.querySelector("#name-card"); // находим инпут ввода заголовка попапа добавления новой карточки
 const popupCardLink = popupCard.querySelector("#link-card"); // находим инпут ввода ссылки попапа добавления новой карточки
 
 const popupImage = document.querySelector("#popup-image");
-//const closeImage = popupImage.querySelector('#image-closer');
+const closeImage = popupImage.querySelector("#image-closer");
 const figure = popupImage.querySelector(".popup__figure-img");
 const figureCaption = popupImage.querySelector(".popup__figure-caption");
-
-//const likeButton = elements.querySelectorAll('.element__like');
 
 const initialCards = [
   {
@@ -51,18 +47,23 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-
-function renderElement(initialMassiveObject) {
-  const templateElements = document
+//создает карточку
+function createCard(initialMassiveObject) {
+  const elementCard = document
     .querySelector("#element-template")
     .content.firstElementChild.cloneNode(true);
-  templateElements.querySelector(".element__image").src =
-    initialMassiveObject.link;
-  templateElements.querySelector(".element__image").alt =
-    initialMassiveObject.name;
-  templateElements.querySelector(".element__caption").textContent =
-    initialMassiveObject.name;
-  elements.prepend(templateElements);
+  const elementCardImage = elementCard.querySelector(".element__image");
+  const elementCardCaption = elementCard.querySelector(".element__caption");
+  elementCardImage.src = initialMassiveObject.link;
+  elementCardImage.alt = initialMassiveObject.name;
+  elementCardCaption.textContent = initialMassiveObject.name;
+  setElementListeners(elementCard);
+  return elementCard;
+}
+
+//добавляет карточку в начало
+function renderCard(card, container) {
+  container.prepend(card);
 }
 
 //добавляет класс .popup_opened
@@ -75,6 +76,7 @@ function closePopup(somePopup) {
   somePopup.classList.remove("popup_opened");
 }
 
+//обрабатывает событие по нажатию на кнопку сохранить
 function formProfileSubmitHandler(evt) {
   evt.preventDefault(); //сборс стандартной отправки
   authorName.textContent = popupProfileAuthorName.value;
@@ -82,34 +84,51 @@ function formProfileSubmitHandler(evt) {
   closePopup(popupProfile);
 }
 
+//обрабатывает событие по нажатию на кнопку сохранить
 function formCardSubmitHandler(evt) {
   evt.preventDefault();
   const newCard = {};
   newCard.name = popupCardHeader.value;
   newCard.link = popupCardLink.value;
-  renderElement(newCard);
+  const createdNewCard = createCard(newCard);
+  renderCard(createdNewCard, elements);
   popupCardHeader.value = "";
   popupCardLink.value = "";
   closePopup(popupCard);
 }
 
-function removeElement(element) {
+function removeElement(evt) {
+  const element = evt.currentTarget.closest(".element");
   element.remove();
 }
 
-function setLikeElement(element) {
+function setLikeElement(evt) {
+  const element = evt.currentTarget.closest(".element");
   const like = element.querySelector(".element__like");
   like.classList.toggle("element__like_active");
 }
 
-function viewElement(element) {
+function viewElement(evt) {
+  const element = evt.currentTarget.closest(".element");
   const elementImg = element.querySelector(".element__image");
   const elementCaption = element.querySelector(".element__caption");
   figure.src = elementImg.src;
+  figure.alt = elementCaption.textContent;
   figureCaption.textContent = elementCaption.textContent;
-  openPopup(popupImage);
+  if (evt.target.classList.contains("element__image")) {
+    openPopup(popupImage);
+  } else if (evt.target.classList.contains("element__caption-group")) {
+    openPopup(popupImage);
+  } else if (evt.target.classList.contains("element__caption")) {
+    openPopup(popupImage);
+  } else if (evt.target.classList.contains("element__ordering")) {
+    openPopup(popupImage);
+  }
 }
-
+/*Здравствуйте. с комментарием который относится к слушателю по всему документу я не совсем согласен. В вебинаре по лайвкодингу, 
+Ян Савицкий как раз нам говорил что устанавливать слушатели в createCard может быть чревато, по тем причинам, 
+что когда на странице будет 1000 элементов, они с созданием карточки будут создаваться и создаваться, соответственно для тысячи картинок будет 3000 слушателей
+как поступать в таком случае? писать всех отдельных слушателей? кого нам слушать (в смысле у кого учиться)? ревьюверов или наставников?
 document.body.addEventListener("click", (evt) => {
   const element = evt.target.closest(".element");
   if (evt.target.classList.contains("element__delete")) {
@@ -124,54 +143,58 @@ document.body.addEventListener("click", (evt) => {
     viewElement(element);
   } else if (evt.target.classList.contains("element")) {
     viewElement(element);
-  } else if (evt.target.classList.contains("profile__edit-button")) {
-    popupProfileAuthorName.value = authorName.textContent;
-    popupProfileAuthorDescription.value = authorDescription.textContent;
-    openPopup(popupProfile);
-  } else if (evt.target.classList.contains("profile__add-button")) {
-    openPopup(popupCard);
-  } else if (evt.target.classList.contains("popup__form-closer_profile")) {
-    closePopup(popupProfile);
-  } else if (evt.target.classList.contains("popup__form-closer_card")) {
-    closePopup(popupCard);
-  } else if (evt.target.classList.contains("popup__form-closer_img")) {
-    closePopup(popupImage);
   }
 });
+*/
+//функция по установке слушателей для элементов карточки
+function setElementListeners(element) {
+  element.querySelector(".element__delete").addEventListener("click", removeElement);
+  element.querySelector(".element__like").addEventListener("click", setLikeElement);
+  //element.querySelector(".element__image").addEventListener("click", viewElement);
+  //element.querySelector(".element__caption-group").addEventListener("click", viewElement);
+  //element.querySelector(".element__caption").addEventListener("click", viewElement);
+  element.querySelector(".element__ordering").addEventListener("click", viewElement);
+}
+//сравнивает места нажатия кликов, если совпадает целевой клик с расположением обработчика, попап закрывается
+function closeByOverlayClick(evt) {
+  const closestPopup = evt.target.closest(".popup");
+  if (evt.target === evt.currentTarget) {
+    closePopup(closestPopup);
+  }
+}
 
-//слушатель по всему попапу, что бы закрывать попап при клике в любом месте, кроме контейнера
-popupProfile.addEventListener(
-  "click",
-  function (event) {
-    if (event.target === event.currentTarget) {
-      //сравнивает места нажатия кликов, если совпадает целевой клик с расположением обработчика, попап закрывается
-      closePopup(popupProfile);
-    }
-  },
-  true
-);
-popupCard.addEventListener(
-  "click",
-  function (event) {
-    if (event.target === event.currentTarget) {
-      closePopup(popupCard);
-    }
-  },
-  true
-);
-popupImage.addEventListener(
-  "click",
-  function (event) {
-    if (event.target === event.currentTarget) {
-      closePopup(popupImage);
-    }
-  },
-  true
-);
+//слушатели на открытие
+profileEditButton.addEventListener("click", function () {
+  popupProfileAuthorName.value = authorName.textContent;
+  popupProfileAuthorDescription.value = authorDescription.textContent;
+  openPopup(popupProfile);
+});
+cardAddButton.addEventListener("click", function () {
+  openPopup(popupCard);
+});
 
-// заполняем секцию elements
-initialCards.map(renderElement);
+//слушатели на закрытие
+closeProfile.addEventListener("click", function () {
+  closePopup(popupProfile);
+});
+closeCard.addEventListener("click", function () {
+  closePopup(popupCard);
+});
+closeImage.addEventListener("click", function () {
+  closePopup(popupImage);
+});
 
 //слушатели-обработчики сабмитов
 formProfile.addEventListener("submit", formProfileSubmitHandler);
 formCard.addEventListener("submit", formCardSubmitHandler);
+
+//слушатель по всему попапу, что бы закрывать попап при клике в любом месте, кроме попапа-контейнера
+popupProfile.addEventListener('mousedown', closeByOverlayClick);
+popupCard.addEventListener('mousedown', closeByOverlayClick);
+popupImage.addEventListener('mousedown', closeByOverlayClick);
+
+// заполняем секцию elements
+initialCards.forEach(function (card) {
+  const createdCard = createCard(card);
+  renderCard(createdCard, elements);
+});
