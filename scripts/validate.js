55d376e0f8278fa0e33ef750b9a72cb0f20ad838
Rushan Shakirzianov <rushan.shakirzianov@gmail.com>
2022-03-29 23:32:32 +0300

    feat: started validation js

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+const profileForm = document.forms.popupFormProfile;// находим форму в попапе профайла
+const profileAuthorName = profileForm.elements.popupInputName;// находим инпут для ввода имени профиля
+const profileAuthorDescription = profileForm.elements.popupInputDescription;// находим инпут для ввода описания профиля
+
+const cardForm = document.forms.popupFormCard; // находим форму попапа добавления новой карточки
+const popupCardHeader = cardForm.elements.popupInputCard;
+const popupCardLink = cardForm.elements.popupInputLink;
+
